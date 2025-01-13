d8c701cbb4bd8e90ebdc6a93bccbd359905a70fc
NandoKe <nandoke@icloud.com>
2025-01-13 13:58:05 +0100

    Build Commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,19 +2,19 @@ import { Component, VERSION, ViewEncapsulation, OnInit } from '@angular/core';
 import lgZoom from 'lightgallery/plugins/zoom';
 import { LightgalleryModule } from 'lightgallery/angular';
 import { BeforeSlideDetail } from 'lightgallery/lg-events';
-import { GoogleAuthService } from '../services/google-auth.service';
-import { GooglePhotosService } from '../services/google-photos.service';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [LightgalleryModule, HttpClientModule],  // Add HttpClientModule here
+  imports: [LightgalleryModule ],  // Add HttpClientModule here
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class PortfolioComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   name = "Angular " + VERSION.major;
   settings = {
     counter: false,
@@ -27,17 +27,4 @@ export class PortfolioComponent implements OnInit {
   };
 
   photos: any[] = [];
-
-  constructor(
-    private authService: GoogleAuthService,
-    private photosService: GooglePhotosService
-  ) {}
-
-  async ngOnInit() {
-    await this.authService.signIn();
-    const accessToken = this.authService.getAccessToken();
-    this.photosService.getPhotos(accessToken).subscribe((data: any) => {
-      this.photos = data.mediaItems;
-    });
-  }
 }
